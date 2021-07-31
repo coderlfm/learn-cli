8b134ddd7a64cab69f23501b81082d2ce6e497c0
宿愿Cc <375642570@qq.com>
2021-07-31 16:49:02 +0800

    feat(core): 完成 输出版本号

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,12 +1,11 @@
 'use strict';
 const { add, log } = require('@sunshine-cli-dev/utils')
+const pkg = require('../package.json')
 
 module.exports = cli;
 function cli(argv) {
   // console.log('sunshine cli', argv);
-  log.success('sunshine cli', '启动成功')
-  console.log(add(10, 209));
+  log.info('sunshinle cli version：', pkg.version)
+  // log.success('sunshine cli', '启动成功')
+  // console.log(add(10, 209));
 }
-// (function cli(argv) {
-//   console.log('sunshine cli', argv);
-// })()
