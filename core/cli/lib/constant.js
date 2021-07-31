e94e0b80dc8bd65b598d0bd5e9c3c35d6d36f750
宿愿Cc <375642570@qq.com>
2021-07-31 18:04:04 +0800

    feat(core): 新增 校验最低 node 版本，新增 semver，colors 库

---
+++ core/cli/lib/constant.js
@@ -0,0 +1,6 @@
+const LOWEST_NODE_VERSION = "16.0.0";
+
+
+module.exports = {
+  LOWEST_NODE_VERSION
+}
--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,11 +1,36 @@
 'use strict';
 const { add, log } = require('@sunshine-cli-dev/utils')
+const semver = require('semver')
+const colors = require('colors')
 const pkg = require('../package.json')
+const constant = require('./constant')
 
 module.exports = cli;
 function cli(argv) {
-  // console.log('sunshine cli', argv);
+  try {
+    checkPkgVersion();
+    checkNodeVersion();
+
+  } catch (error) {
+    // 自定义错误处理
+    log.error(error.message)
+  }
+}
+
+/**
+ * 校验最低 node 版本
+ */
+function checkNodeVersion() {
+  const currentVersion = process.version;
+  const lowestVsrion = constant.LOWEST_NODE_VERSION;
+
+  if (semver.gte(lowestVsrion, currentVersion)) {
+    throw new Error(colors.red(`sunshine cli 需要安装 v${lowestVsrion} 以上的 node 版本`))
+  }
+}
+
+function checkPkgVersion() {
   log.info('sunshinle cli version：', pkg.version)
-  // log.success('sunshine cli', '启动成功')
-  // console.log(add(10, 209));
+  log.verbose('test', 'debugger')
+
 }
