c4167699cba7b057ccf2f6be1608e681ccedb1f4
宿愿Cc <375642570@qq.com>
2021-08-01 15:23:19 +0800

    feat(core): 新增 node中使用 es Module 的方式2 .mjs

--- core/cli/bin/index.js
+++ core/cli/bin/index.js
@@ -4,13 +4,14 @@
 // console.log('hello sunshine cli');
 const importlocal = require('import-local')
 const { log } = require('@sunshine-cli-dev/utils')
+import utils from '../lib/utils.mjs'
 
 if (importlocal(__dirname)) {
   log.info('sunshile cli', '加载本地模块')
 } else {
-
+  utils()
   // console.log(process.versions);
 
   // require('../lib')(process.argv.slice(2))
-  require('../dist')
+  // require('../dist')
 }
---
+++ core/cli/bin/index.mjs
@@ -0,0 +1,6 @@
+#! /usr/bin/env node
+
+'use strict';
+// console.log('hello sunshine cli');
+import utils from '../lib/utils.mjs'
+utils()
